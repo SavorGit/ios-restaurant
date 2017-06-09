427ffce553406091c82102dc51c60d530bbcd449
郭春城 <gccios@sina.com>
2017-06-09 11:12:36 +0800

    热点儿餐厅端1.0版本

---
+++ HotTopicsForRestaurant/HotTopicsForRestaurantTests/HotTopicsForRestaurantTests.m
@@ -0,0 +1,39 @@
+//
+//  HotTopicsForRestaurantTests.m
+//  HotTopicsForRestaurantTests
+//
+//  Created by 郭春城 on 2017/6/9.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HotTopicsForRestaurantTests : XCTestCase
+
+@end
+
+@implementation HotTopicsForRestaurantTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
