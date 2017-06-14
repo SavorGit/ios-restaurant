0a9df360f14f184ca725c443280df74dcdd46a6a
beimingyouyu0823 <haipeng0823@126.com>
2017-06-14 10:37:29 +0800

    修改部分逻辑

---
+++ HotTopicsForRestaurant/HotTopicsForRestaurant/Views/OtherViews/ConnectMaskingView.h
@@ -0,0 +1,15 @@
+//
+//  ConnectMaskingView.h
+//  HotTopicsForRestaurant
+//
+//  Created by 王海朋 on 2017/6/14.
+//  Copyright © 2017年 郭春城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnectMaskingView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame;
+
+@end
