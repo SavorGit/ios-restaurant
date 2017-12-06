5f40651077de7f1a459e906b0736014120bb99a5
beimingyouyu0823 <haipeng0823@126.com>
2017-12-06 11:33:29 +0800

    修改

--- HotTopicsForRestaurant/HotTopicsForRestaurant/Controllers/RecommendedDishes/SelectRoomViewController.m
+++ HotTopicsForRestaurant/HotTopicsForRestaurant/Controllers/RecommendedDishes/SelectRoomViewController.m
@@ -90,10 +90,6 @@
     }];
 }
 
--(void)toScreenBtnDidClicked:(UIButton *)Btn{
-    
-}
-
 #pragma mark - UICollectionView 代理方法
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
     return 1;
@@ -130,8 +126,7 @@
     if (self.backDatas) {
         self.backDatas(tmpModel);
     }
-    [self dismissViewControllerAnimated:YES completion:^{
-    }];
+    [self back];
     
 }
 
