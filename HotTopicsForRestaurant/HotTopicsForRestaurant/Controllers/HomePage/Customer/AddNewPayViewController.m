faf4012d881f8e87a9475e046b8f8ad408fbd01c
郭春城 <gccios@sina.com>
2017-12-27 11:08:05 +0800

    修改工程

--- HotTopicsForRestaurant/HotTopicsForRestaurant/Controllers/HomePage/Customer/AddNewPayViewController.m
+++ HotTopicsForRestaurant/HotTopicsForRestaurant/Controllers/HomePage/Customer/AddNewPayViewController.m
@@ -7,14 +7,22 @@
 //
 
 #import "AddNewPayViewController.h"
-#import "NewPayTagCell.h"
+#import "CustomerListViewController.h"
 
 @interface AddNewPayViewController ()<UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView * tableView;
 
 @property (nonatomic, strong) UIView * topView;
+@property (nonatomic, strong) UITextField * firstTelField; //手机号
+@property (nonatomic, strong) UITextField * nameField; //姓名
+
+@property (nonatomic, strong) UIImageView * logoImageView;
+@property (nonatomic, strong) UILabel * logoLabel;
+
 @property (nonatomic, strong) UIView * bottomView;
+
+@property (nonatomic, strong) NSMutableArray * tagSource;
 
 @end
 
@@ -24,23 +32,118 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    [self createAddNewPayView];
+}
+
+- (void)createAddNewPayView
+{
+    CGFloat scale = kMainBoundsWidth / 375.f;
+    self.topView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kMainBoundsWidth, 200 * scale)];
+    self.topView.backgroundColor = [UIColor whiteColor];
+    
+    self.nameField = [self textFieldWithPlaceholder:@"请输入姓名" leftImageNamed:@""];
+    [self.topView addSubview:self.nameField];
+    [self.nameField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(30 * scale);
+        make.left.mas_equalTo(15 * scale);
+        make.height.mas_equalTo(30 * scale);
+        make.width.mas_equalTo(kMainBoundsWidth-125 * scale);
+    }];
+    
+    self.firstTelField = [self textFieldWithPlaceholder:@"请输入手机号" leftImageNamed:@""];
+    [self.topView addSubview:self.firstTelField];
+    [self.firstTelField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.nameField.mas_bottom).offset(30 * scale);
+        make.left.mas_equalTo(15 * scale);
+        make.height.mas_equalTo(30 * scale);
+        make.width.mas_equalTo(kMainBoundsWidth-125 * scale);
+    }];
+    
+    UIButton * logoButton = [Helper buttonWithTitleColor:UIColorFromRGB(0xffffff) font:kPingFangRegular(14 * scale) backgroundColor:[UIColor clearColor] title:@""];
+    [logoButton addTarget:self action:@selector(logoButtonDidClicked) forControlEvents:UIControlEventTouchUpInside];
+    [self.topView addSubview:logoButton];
+    [logoButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(0);
+        make.left.mas_equalTo(self.nameField.mas_right);
+        make.bottom.mas_equalTo(self.firstTelField.mas_bottom);
+        make.width.mas_equalTo(110 * scale);
+    }];
+    
+    self.logoImageView = [[UIImageView alloc] init];
+    [logoButton addSubview:self.logoImageView];
+    [self.logoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(40 * scale);
+        make.centerX.mas_equalTo(0);
+        make.width.height.mas_equalTo(50 * scale);
+    }];
+    self.logoImageView.backgroundColor = [UIColor grayColor];
+    
+    self.logoLabel = [Helper labelWithFrame:CGRectZero TextColor:UIColorFromRGB(0x333333) font:kPingFangRegular(14 * scale) alignment:NSTextAlignmentCenter];
+    self.logoLabel.text = @"选择客户";
+    [logoButton addSubview:self.logoLabel];
+    [self.logoLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(0);
+        make.width.mas_equalTo(90 * scale);
+        make.height.mas_equalTo(15 * scale);
+        make.top.mas_equalTo(self.logoImageView.mas_bottom).offset(10 *scale);
+    }];
+    
     self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     self.tableView.backgroundColor = [UIColor clearColor];
     self.tableView.showsVerticalScrollIndicator = NO;
-    [self.tableView registerClass:[NewPayTagCell class] forCellReuseIdentifier:@"NewPayTagCell"];
-    self.tableView.estimatedRowHeight = 120.f;
-    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+    
+    self.tableView.tableHeaderView = self.topView;
+    
     [self.view addSubview:self.tableView];
     [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.edges.mas_equalTo(0);
     }];
+}
+
+- (void)logoButtonDidClicked
+{
+    CustomerListViewController * list = [[CustomerListViewController alloc] init];
+    [self.navigationController pushViewController:list animated:YES];
+}
+
+- (UITextField *)textFieldWithPlaceholder:(NSString *)placeholder leftImageNamed:(NSString *)imageName
+{
+    UITextField * textField = [[UITextField alloc] initWithFrame:CGRectZero];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self.tableView reloadData];
-    });
+    CGFloat scale = kMainBoundsWidth / 375.f;
+    
+    textField.font = kPingFangRegular(15.f * scale);
+    textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    textField.textColor = UIColorFromRGB(0x333333);
+    
+    UIView * leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 32 * scale, 18 * scale)];
+    UIImageView * leftImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 18 * scale, 18 * scale)];
+    [leftImageView setImage:[UIImage imageNamed:imageName]];
+    [leftView addSubview:leftImageView];
+    
+    textField.leftView = leftView;
+    textField.leftViewMode = UITextFieldViewModeAlways;
+    
+    NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:placeholder attributes:
+                                      @{NSForegroundColorAttributeName:UIColorFromRGB(0x999999),
+                                        NSFontAttributeName:kHiraginoSansW3(15.f * scale)
+                                        }];
+    textField.attributedPlaceholder = attrString;
+    
+    UIView * line = [[UIView alloc] initWithFrame:CGRectZero];
+    line.backgroundColor = [UIColor grayColor];
+    [textField addSubview:line];
+    [line mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(25 * scale);
+        make.bottom.right.mas_equalTo(0);
+        make.height.mas_equalTo(.5f);
+    }];
+    
+    return textField;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -50,9 +153,25 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NewPayTagCell * cell = [tableView dequeueReusableCellWithIdentifier:@"NewPayTagCell" forIndexPath:indexPath];
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    
+    cell.backgroundColor = UIColorFromRGB(0xece6de);
     
     return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat scale = kMainBoundsWidth / 375.f;
+    return 10.f * scale;
+}
+
+- (NSMutableArray *)tagSource
+{
+    if (!_tagSource) {
+        _tagSource = [[NSMutableArray alloc] initWithObjects:@"偏辣", @"爱吃海鲜", @"不喜欢香菜", @"好面子", @"喜欢推荐菜", @"投屏", @"喜欢漂亮的妹子", nil];
+    }
+    return _tagSource;
 }
 
 - (void)didReceiveMemoryWarning {
