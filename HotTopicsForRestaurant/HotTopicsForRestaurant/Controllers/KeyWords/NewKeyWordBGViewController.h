c34d0187eefb51b616d8e5717fd537abfd3740a3
郭春城 <gccios@sina.com>
2018-01-31 17:46:58 +0800

    增加部分功能

---
+++ HotTopicsForRestaurant/HotTopicsForRestaurant/Controllers/KeyWords/NewKeyWordBGViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewKeyWordBGViewController.h
+//  HotTopicsForRestaurant
+//
+//  Created by 郭春城 on 2018/1/31.
+//  Copyright © 2018年 郭春城. All rights reserved.
+//
+
+#import "ResBaseViewController.h"
+
+@interface NewKeyWordBGViewController : ResBaseViewController
+
+@end
